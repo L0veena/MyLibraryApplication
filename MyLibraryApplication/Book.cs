a5b37f6ac6151c709bcbce9bafdf8265d621e22a
Loveena <Loveenalukose@gmail.com>
2016-07-22 23:33:51 -0700

    Book and Member class with just properties

---
+++ MyLibraryApplication/Book.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLibraryApplication
+{
+    /// <summary>
+    /// This class defines a Book
+    /// </summary>
+     public class Book
+    {
+
+        #region Properties
+
+        /// <summary>
+        /// ISBN number of the Book
+        /// </summary>
+        public int IsbnNumber { get; set; }
+
+        /// <summary>
+        /// Title of the book
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// Author of the Book
+        /// </summary>
+        public string Author  { get; set; }
+
+        /// <summary>
+        /// Genre of the Book
+        /// </summary>
+        public string Type { get; set; }
+
+        /// <summary>
+        /// Publisher of the Book
+        /// </summary>
+        public string Publisher { get; set; }
+
+        /// <summary>
+        /// Date of publication of the book
+        /// </summary>
+        public DateTime DateOfPublication { get; set; }
+
+        #endregion
+    }
+}
---
+++ MyLibraryApplication/Member.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLibraryApplication
+{
+    /// <summary>
+    /// This class defines a Member
+    /// </summary>
+    public class Member
+    {
+        #region Properties
+        /// <summary>
+        /// MemberID
+        /// </summary>
+        public int MemberId { get; set; }
+
+        /// <summary>
+        /// Member Name
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Member's Email address
+        /// </summary>
+        public string EmailAddress { get; set; }
+
+        /// <summary>
+        /// Memeber's Address
+        /// </summary>
+        public string Address  { get; set; }
+
+        /// <summary>
+        /// Member's PhoneNumber
+        /// </summary>
+        public int PhoneNumber { get; set; }
+
+        #endregion
+
+    }
+}
